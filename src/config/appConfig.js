bb95916f7eeab0fe79f6a7528e5d83014f3f1d51
Panupong-xD <pccchon05706_64@pccchon.ac.th>
2026-08-08 21:54:38 +0700

    chore: update .gitignore to include .env file; clear default URLs in appConfig for security

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -4,7 +4,7 @@
  */
 
 // Google Sheet Published CSV URL (for real-time order status querying)
-export const GOOGLE_SHEET_CSV_URL = import.meta.env.VITE_GOOGLE_SHEET_CSV_URL || 'https://docs.google.com/spreadsheets/d/1EivWlwAwzhoMYdj-OT_MfcL-mlVaPUMuFQiAIHGtlJM/edit?usp=sharing';
+export const GOOGLE_SHEET_CSV_URL = import.meta.env.VITE_GOOGLE_SHEET_CSV_URL || '';
 
 // Google Form URL (for ordering shirts / registering)
-export const GOOGLE_FORM_URL = import.meta.env.VITE_GOOGLE_FORM_URL || 'https://docs.google.com/forms/d/e/1FAIpQLSeP-OP7ixNw6rkWrb9c2JQyZQQpDx8-DT6ndHUNtihWhcJagA/viewform?usp=sharing&ouid=106892660776885504222';
+export const GOOGLE_FORM_URL = import.meta.env.VITE_GOOGLE_FORM_URL || '';
